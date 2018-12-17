ff9c4f3566403aa30eb4439f6778a520882e569a
Spencer Hilvitz <spencer.hilvitz@wipro.com>
2018-12-17 14:12:48 -0700

    Learn about different selectors

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  // selector: 'app-servers',
+  selector: '[app-servers]', // turns selecting an element into selecting an attribute on an element...works for css??
+  // template: '<app-server></app-server><app-server></app-server>',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
