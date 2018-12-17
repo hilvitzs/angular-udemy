ee1b5f8bdad36085cbd32d3429ab2d13d5c3f3a5
Spencer Hilvitz <spencer.hilvitz@wipro.com>
2018-12-17 14:13:22 -0700

    Change commented out lines

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  // selector: 'app-servers',
-  selector: '[app-servers]', // turns selecting an element into selecting an attribute on an element...works for css??
+  selector: 'app-servers',
+  // selector: '[app-servers]', turns selecting an element into selecting an attribute on an element...works for css??
   // template: '<app-server></app-server><app-server></app-server>',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
