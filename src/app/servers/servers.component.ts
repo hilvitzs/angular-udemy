2a96262db21bf50dbc9b25183a6d1a4d03b459c1
Spencer Hilvitz <spencer.hilvitz@wipro.com>
2018-12-17 14:16:49 -0700

    Add options

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  // selector: '[app-servers]', turns selecting an element into selecting an attribute on an element...works for css??
+  selector: 'app-servers', // element selector
+  // selector: '[app-servers]', // attribute selector
+  // selector: '.app-servers', // class selector
   // template: '<app-server></app-server><app-server></app-server>',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
